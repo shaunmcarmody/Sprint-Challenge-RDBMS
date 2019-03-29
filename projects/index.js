ba33969babe1f45a77a8d787785b7d7c0b3a32a9
shaunmcarmody <shaun.carmody@live.co.uk>
2019-03-29 18:42:17 +0000

    run all crud operations for actions and projects

--- projects/index.js
+++ projects/index.js
@@ -41,6 +41,41 @@ router.post('/', (req, res) => {
     });
 })
 
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db('projects')
+    .where({ id })
+    .del()
+    .then(project => {
+      console.log(project)
+      if (project) {
+        res.status(200).json({ message: 'Project deleted' });
+      } else {
+        res.status(404).json({ message: 'Project not found'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  db('projects')
+    .where({ id })
+    .update(req.body)
+    .then(project => {
+      if (project) {
+        res.status(200).json({ message: 'Project updated' });
+      } else {
+        res.status(404).json({ message: 'Project not found'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+})
+
 
 
 module.exports = router;
